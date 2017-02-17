174362088eee444facaf7134162857e7fbf99a70
Nitesh Kachroo <niteshkac@gmail.com>
2017-02-17 12:48:54 +0530

    My first test with chai mocha

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by niccu on 17/2/17.
+ */
+
+const { expect } = require('chai');
+
+describe('A basic test', function () {
+  it('should pass when everything is okay', function () {
+    expect(true).to.be.true;
+  });
+});
